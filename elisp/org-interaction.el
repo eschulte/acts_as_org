9f90d6617bab92036b7926846a0b58f405157155
Francesco Pizzolante <francesco.pizzolante@gmail.com>
2009-11-23 08:28:57 -0700

    added `org-file-to-pdf' for automated export of org-mode documents to pdf

--- elisp/org-interaction.el
+++ elisp/org-interaction.el
@@ -65,6 +65,31 @@ latex as a string."
 	 (write-file latex-path)
 	 (kill-buffer (current-buffer)))))))
 
+(defun org-file-to-pdf (file-path)
+  "Open up an org file and export it as pdf."
+  (let* ((file-name (file-name-nondirectory file-path))
+        (file-dir (file-name-directory file-path))
+        (org-tmp-path (make-temp-file "org-file-to-pdf-"))
+        (pdf-tmp-path (concat org-tmp-path ".pdf"))
+        (tex-tmp-path (concat org-tmp-path ".tex"))
+        (pdf-path (expand-file-name (concat org-interaction-prefix file-name ".pdf") file-dir)))
+    (if (and (file-exists-p pdf-path)
+            (< 0 (time-to-seconds
+                  (time-subtract
+                   (nth 5 (file-attributes pdf-path))
+                   (nth 5 (file-attributes file-path))))))
+       pdf-path
+      (with-temp-filebuffer
+       file-path
+       (write-file org-tmp-path)
+       (org-mode)
+       (save-window-excursion
+        (org-export-as-pdf nil)
+        (rename-file pdf-tmp-path pdf-path t)
+        (delete-file org-tmp-path)
+        (delete-file tex-tmp-path)
+        (kill-buffer (current-buffer)))))))
+
 ;; customization
 (setq org-export-blocks-witheld '(hidden comment))
 
